da922a6dc85cbb25e38af479c93770ebdd09eedb
Michael Yu <michael.yu2016@gmail.com>
2018-05-02 14:05:38 -0700

    restructure getElement

--- src/containers/display.js
+++ src/containers/display.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import Button from '../components/Button';
-import COMPONENTS from '../config/config';
+import COMPONENTS, { getElement } from '../config/config';
 
 class Display extends React.Component {
   constructor(props) {
@@ -24,26 +23,6 @@ class Display extends React.Component {
   }
 
   render() {
-    const onClickNull
-      = () => {};
-
-    const getElement = (s, i) => {
-      let result;
-      switch (s) {
-        case 'Button':
-          result = <Button key={i} label="button" color="primary" onClick={onClickNull} />;
-          break;
-        case 'Card':
-          result = <Button key={i} label="card" color="primary" onClick={onClickNull} />;
-          break;
-        default:
-          result = <Button key={i} label="default" color="primary" onClick={onClickNull} />;
-          break;
-      }
-
-      return result;
-    };
-
     return (
       <div className="display">
         {this.state.components.map((component, index) => (
