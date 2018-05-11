486a7d4788d8c2ada412922059c791db28af8d59
Michael Yu <michael.yu2016@gmail.com>
2018-05-11 15:44:25 -0700

    made inline stepper

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Button from '../components/Button';
+import InlineStepper from '../components/InlineStepper';
 
 const COMPONENTS = [
   'Button',
   'Card',
+  'InlineStepper',
 ];
 
 const onClickNull = () => {};
@@ -17,6 +19,9 @@ export const getElement = (s, i) => {
       break;
     case 'Card':
       result = <Button key={i} label="card" color="primary" onClick={onClickNull} />;
+      break;
+    case 'InlineStepper':
+      result = <InlineStepper key={i} label="inline stepper" />;
       break;
     default:
       result = <Button key={i} label="default" color="primary" onClick={onClickNull} />;
