10ff435e1d60c24e3be7cfd22aab887c3415aa7a
Michael Yu <michael.yu2016@gmail.com>
2018-05-11 18:49:20 -0700

    created paragraph cmpnt

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 import Button from '../components/Button';
-import InlineStepper from '../components/InlineStepper';
 import Image from '../components/Image';
+import InlineStepper from '../components/InlineStepper';
+import Paragraph from '../components/Paragraph';
 
 const COMPONENTS = [
   'Button',
   'Card',
   'InlineStepper',
   'Image',
+  'Paragraph'
 ];
 
 const onClickNull = () => {};
+
+const pgText = 'Lorem ipsum dolor sit amet, vis populo laoreet indoctum ne, per ad minim feugiat omittam, quas habeo numquam ad eam. Eu invidunt expetenda usu. No tollit utamur adolescens vel. No vocibus placerat sapientem mei, ei vix nemore accusata patrioque. Impetus concludaturque cu qui, veri prima qui ut, causae scripserit ne pro.';
 
 export const getElement = (s, i) => {
   let result;
@@ -27,6 +31,9 @@ export const getElement = (s, i) => {
       break;
     case 'Image':
       result = <Image key={i} />;
+      break;
+    case 'Paragraph':
+      result = <Paragraph key={i} text={pgText} />;
       break;
     default:
       result = <Button key={i} label="default" color="primary" onClick={onClickNull} />;
