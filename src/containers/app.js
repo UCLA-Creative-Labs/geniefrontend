2e102d231f272e03ade8fe543f4f38ee53bba2af
Bryan Wong <bryanw0ng@ucla.edu>
2018-05-29 21:14:16 -0700

    upload css

--- src/containers/app.js
+++ src/containers/app.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom';
 
 import Home from './home';
 import Display from './display';
+import Upload from './upload';
 
 const App = () => (
   <div>
     <Route exact path="/" component={Home} />
     <Route exact path="/display" component={Display} />
+    <Route exact path="/upload" component={Upload} />
   </div>
 );
 
