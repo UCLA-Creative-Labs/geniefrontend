019420e5fa46c689cef81f05dfb2206fcb645558
Bryan Wong <bryanw0ng@ucla.edu>
2018-06-03 16:13:52 -0700

    add bg to upload

--- src/containers/upload.js
+++ src/containers/upload.js
@@ -6,6 +6,8 @@ import Heading from '../components/Heading';
 import Paragraph from '../components/Paragraph';
 import { getElement } from '../config/config';
 
+import bg from '../assets/images/bg-gradient-2.png';
+
 class Upload extends React.Component {
   constructor(props) {
     super(props);
@@ -41,12 +43,15 @@ class Upload extends React.Component {
         <div className="page upload">
           <div className="upload-container">
           	<div className="upload-heading">
-          		<Heading headingLevel="1" content="Upload your image" />
+          		<Heading headingLevel={1} content="Upload your image" />
           		<Paragraph text="Watch your ideas come to life." />
           	</div>
            <div className="upload-dropzone">
            	 <UploadBox className="upload-dropzone" setComponents={this.setComponents} label="Drag files to upload" />
            </div>
+          </div>
+          <div className="bg-container">
+            <img alt="bg" src={bg} />
           </div>
         </div>
       </div>
